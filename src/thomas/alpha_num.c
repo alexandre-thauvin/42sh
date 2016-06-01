0752c419bb06bf8371c552d1c10ce1b84e71c6d7
Thomas Lavigne <thomas.lavigne@epitech.eu>
2016-06-01 10:40:08 +0200

    alpha_num.c update norm

--- src/thomas/alpha_num.c
+++ src/thomas/alpha_num.c
@@ -5,17 +5,37 @@
 ** Login   <lavign_t@epitech.net>
 ** 
 ** Started on  Tue May 31 15:21:41 2016 thomas lavigne
-** Last update Tue May 31 18:05:44 2016 thomas lavigne
+** Last update Wed Jun  1 10:39:01 2016 thomas lavigne
 */
 
 #include "shell.h"
 
+int	alpha_num2(char **arg, t_second *ini, int i)
+{
+  int	x;
+
+  x = 0;
+  while (arg[i][x] != 0)
+    {
+      if ((arg[i][x] < '0' || arg[i][x] > '9') &&
+	  (arg[i][x] < 'a' || arg[i][x] > 'z') &&
+	  (arg[i][x] < 'A' || arg[i][x] > 'Z'))
+	{
+	  fprintf(stderr, "setenv: Variable name must contain ");
+	  fprintf(stderr, "alphanumeric characters.\n");
+	  ini->check = -1;
+	  return (-1);
+	}
+      x++;
+    }
+  return (0);
+}
+
 int	alpha_num(char **arg, t_second *ini)
 {
   int	i;
-  int	x;
 
-  x = i = 0;
+  i = 0;
   while (arg && arg[i] != NULL)
     {
       if ((arg[i][0] < 'a' || arg[i][0] > 'z') &&
@@ -26,20 +46,8 @@ int	alpha_num(char **arg, t_second *ini)
 	    ini->check = -1;
 	    return (-1);
 	  }
-      while (arg[i][x] != 0)
-      	{
-      	  if ((arg[i][x] < '0' || arg[i][x] > '9') &&
-      	      (arg[i][x] < 'a' || arg[i][x] > 'z') &&
-      	      (arg[i][x] < 'A' || arg[i][x] > 'Z'))
-	    {
-	      fprintf(stderr, "setenv: Variable name must contain ");
-	      fprintf(stderr, "alphanumeric characters.\n");
-	      ini->check = -1;
-	      return (-1);
-	    }
-      	  x++;
-      	}
-      x = 0;
+      if (alpha_num2(arg, ini, i) == -1)
+	return (-1);
       i++;
     }
   return (0);
