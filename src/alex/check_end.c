b1469f5fa5ed84458ba89582b27c66c46fb95f73
Thomas Lavigne <thomas.lavigne@epitech.eu>
2016-06-05 21:37:56 +0200

    no existant bin

--- src/alex/check_end.c
+++ src/alex/check_end.c
@@ -5,7 +5,7 @@
 ** Login   <lavign_t@epitech.net>
 ** 
 ** Started on  Sun Jun  5 13:46:55 2016 thomas lavigne
-** Last update Sun Jun  5 13:48:06 2016 thomas lavigne
+** Last update Sun Jun  5 21:34:09 2016 thomas lavigne
 */
 
 #include "shell.h"
@@ -13,7 +13,7 @@
 int	check_end(t_second *ini)
 {
   if (ini->error.check2 == -1 || ini->error.check == -1
-      || ini->pip.redir == -1)
+      || ini->pip.redir == -1 || ini->error.status != 0)
     return (-1);
   return (0);
 }
